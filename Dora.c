5611643e2e995c41606e2e19d8a72d1e17261195
Baki Kesici <kesicibaki@gmail.com>
2024-10-20 16:59:36 +0300

    second commit

--- Dora.c
+++ Dora.c
@@ -1,101 +1,112 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 struct LocationData {
     time_t currentTime;
-    int latitude;      
-    int longitude;     
+    int latitude;   // Latitude integer olarak
+    int longitude;  // Longitude integer olarak
     int altitude;
 } typedef LocationData;
 
+// Konumu mesaj formatına çeviren fonksiyon
 char* locationToMessage(LocationData location) {
     char* message = (char*)malloc(100 * sizeof(char));
+    if (message == NULL) {
+        perror("Bellek ayırma başarısız");
+        exit(EXIT_FAILURE);
+    }
     snprintf(message, 100, "Time: %ld, Latitude: %d, Longitude: %d, Altitude: %d",
              location.currentTime, location.latitude, location.longitude, location.altitude);
     return message;
-
-const char *encrypt(const char *message, int shift) {
-    int len = strlen(message);
-    char* encryptMessage = (char*)malloc((len + 1) * sizeof(char));
-    int i;
-    
-    for(i=0 ; message[i] != '\0'; i++){
-        char ch = message[i];
-        if(ch => 'a' && ch <= 'z'){
-            encryptMessage[i] = ((ch - 'a' + shift) %26) + 'a';
-        }
-        else if(ch => 'A' && ch <= 'Z'){
-            encryptMessage[i] = ((ch - 'A' + shift) %26) + 'A';
-        }
-        else{
-            encryptMessage[i] = ch;
-        }
-    }
 }
 
+// Mesajı şifreleyen fonksiyon (Sezar şifreleme yöntemi)
+char* encrypt(const char* message, int shift) {
+    int len = strlen(message);
+    char* encryptMessage = (char*)malloc((len + 1) * sizeof(char));
+    if (encryptMessage == NULL) {
+        perror("Bellek ayırma başarısız");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < len; i++) {
+        char ch = message[i];
+        if (ch >= 'a' && ch <= 'z') {
+            encryptMessage[i] = ((ch - 'a' + shift) % 26) + 'a';  // Küçük harfler için
+        } else if (ch >= 'A' && ch <= 'Z') {
+            encryptMessage[i] = ((ch - 'A' + shift) % 26) + 'A';  // Büyük harfler için
+        } else {
+            encryptMessage[i] = ch;  // Diğer karakterler için
+        }
+    }
+    encryptMessage[len] = '\0';  // Sonuna null karakter ekle
+    return encryptMessage;
+}
+
+// Konum bilgisi üreten fonksiyon
 LocationData getLocation() {
     time_t currentTime = time(NULL);
-    int latitude = rand() % 180 - 90;
-    int longitude = rand() % 360 - 180;
-    int altitude = rand() 5000;
+    int latitude = rand() % 180 - 90;  // Latitude değeri (-90, 90) arası
+    int longitude = rand() % 360 - 180;  // Longitude değeri (-180, 180) arası
+    int altitude = rand() % 5000;  // Rastgele bir altitude değeri
+
     LocationData location = {currentTime, latitude, longitude, altitude};
     return location;
 }
 
-void sendMessage(const char *encryptedMessage,const char *serverIP, int serverPort) {
-
+// Şifrelenmiş mesajı sunucuya gönderen fonksiyon
+void sendMessage(const char* encryptedMessage, const char* serverIP, int serverPort) {
     int sockfd;
     struct sockaddr_in serverAddr;
 
-    // 1. UDP soketi oluşturun
+    // UDP soketi oluştur
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
-        perror("Socket creation failed");
+        perror("Socket oluşturma başarısız");
         exit(EXIT_FAILURE);
     }
 
-    // 2. Sunucu adres yapısını doldurun
-    memset(&serverAddr, 0, sizeof(serverAddr));  // Adres yapısını sıfırlama
-    serverAddr.sin_family = AF_INET;             // IPv4
-    serverAddr.sin_port = htons(serverPort);     // Sunucu portu
-    serverAddr.sin_addr.s_addr = inet_addr(serverIP); // Sunucu IP adresi
+    // Sunucu adresini yapılandır
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP);
 
-    // 3. Şifrelenmiş mesajı UDP ile gönderin
+    // Mesajı gönder
     int n = sendto(sockfd, encryptedMessage, strlen(encryptedMessage), 0, 
-                   (const struct sockaddr *) &serverAddr, sizeof(serverAddr));
-    if (n < 0) {
-        perror("Send failed");
+                   (const struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (n == -1) {
+        perror("Mesaj gönderme başarısız");
     } else {
-        printf("Encrypted message sent to %s:%d\n", serverIP, serverPort);
+        printf("Şifreli mesaj gönderildi: %s\n", encryptedMessage);
     }
 
-    // 4. Soketi kapatın
-    close(sockfd);
-    
-
+    close(sockfd);  // Soketi kapat
 }
 
 int main() {
-    LocationData location = getLocation();
+    srand(time(NULL));  // Rastgelelik için zaman değeri
+    LocationData location = getLocation();  // Konumu al
 
-    char* message= locationToMessage(location);
-    printf("Mesaj: %s\n",message);
+    char* message = locationToMessage(location);  // Mesajı oluştur
+    printf("Mesaj: %s\n", message);
 
-    int shift = 6;
-    char* encryptMessage = encrypt(message,shift);
-    printf("Şifreli Mesaj: %s\n",encryptMessage);
+    int shift = 6;  // Şifreleme kaydırma miktarı
+    char* encryptedMessage = encrypt(message, shift);  // Mesajı şifrele
+    printf("Şifreli Mesaj: %s\n", encryptedMessage);
 
-    const char *serverIP = "127.0.0.1";
-    int serverPort = 7000;
+    const char* serverIP = "127.0.0.1";  // Groundstation IP'si (localhost)
+    int serverPort = 7000;  // Groundstation portu
 
-    sendMessage(encryptMessage,serverIP,serverPort);
-    free(meesage);
-    free(encryptMessage);
+    sendMessage(encryptedMessage, serverIP, serverPort);  // Mesajı gönder
 
+    free(message);  // Belleği temizle
+    free(encryptedMessage);  // Belleği temizle
 
     return 0;
 }
--- GroundStation.c
+++ GroundStation.c
@@ -1,23 +1,91 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
 
+// Mesajı almak için bir fonksiyon
+const char* receiveMessage(int port) {
+    static char buffer[1024];
+    int sockfd;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t addrLen = sizeof(clientAddr);
 
-const char* receiveMessage() {
-    // mesaji alacak fonksiyon
-    
+    // Socket oluştur
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Socket oluşturma başarisiz");
+        exit(EXIT_FAILURE);
+    }
+
+    // Sunucu adresini ayarla
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;  // Tüm arayüzlerden gelen bağlantıları kabul et
+    serverAddr.sin_port = htons(port);  // Port numarasını ayarla
+
+    // Socket'i bağla
+    if (bind(sockfd, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Bind başarısız");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Mesajı al
+    int n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0, 
+                     (struct sockaddr *) &clientAddr, &addrLen);
+    if (n < 0) {
+        perror("Alma başarısız");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[n] = '\0';  // Null karakter ile sonlandır
+
+    close(sockfd);  // Soketi kapat
+
+    return buffer;  // Alınan mesajı döndür
 }
 
+// Mesajı şifre çözmek için bir fonksiyon
 const char* decrypt(const char* encryptedMessage) {
-    // mesajin sifresini cozecek fonksiyon
+    int shift = 6;  // Şifre çözme kaydırma miktarı
+    int len = strlen(encryptedMessage);
+    char* decryptMessage = (char*)malloc((len + 1) * sizeof(char));
+    if (decryptMessage == NULL) {
+        perror("Bellek ayırma başarısız");
+        exit(EXIT_FAILURE);
+    }
 
+    for (int i = 0; i < len; i++) {
+        char ch = encryptedMessage[i];
+        if (ch >= 'a' && ch <= 'z') {
+            decryptMessage[i] = ((ch - 'a' - shift + 26) % 26 + 'a');  // Küçük harfler için
+        } else if (ch >= 'A' && ch <= 'Z') {
+            decryptMessage[i] = ((ch - 'A' - shift + 26) % 26 + 'A');  // Büyük harfler için
+        } else {
+            decryptMessage[i] = ch;  // Diğer karakterler için
+        }
+    }
+    decryptMessage[len] = '\0';  // Null karakter ile sonlandır
+    return decryptMessage;  // Çözülen mesajı döndür
 }
 
+// Mesajı ekrana yazdıran bir fonksiyon
 void displayMessage(const char* message) {
-    // mesaji yazdiracak fonksiyon
-
+    printf("Çözülmüş şifre: %s\n", message);
 }
 
 int main() {
-    
-    return 0; 
-} 
+    int serverPort = 7000;  // Sunucu portu
+    const char* encryptedMessage = receiveMessage(serverPort);  // Mesajı al
+    printf("Alınan şifreli mesaj: %s\n", encryptedMessage);  // Alınan mesajı yazdır
+
+    const char* decryptedMessage = decrypt(encryptedMessage);  // Şifreyi çöz
+    displayMessage(decryptedMessage);  // Çözülmüş mesajı göster
+
+    free((void*)decryptedMessage);  // Çözülmüş mesaj için belleği serbest bırak
+
+    return 0;
+}
